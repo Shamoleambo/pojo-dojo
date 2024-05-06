f63dad5208689f417dc380d766b6d32091b6f683
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-05-06 14:28:46 -0300

    first commit

---
+++ src/main/java/com/tidcode/pojodojo/PojoDojoApplication.java
@@ -0,0 +1,13 @@
+package com.tidcode.pojodojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojoDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojoDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidcode/pojodojo/PojoDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidcode.pojodojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojoDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
