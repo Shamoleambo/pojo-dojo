270fbef855fcba106ee9620912a56f1d9b5eed21
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-05-06 15:30:52 -0300

    create PersonErrorResponse class

--- src/main/java/com/tidcode/pojodojo/controller/TheController.java
+++ src/main/java/com/tidcode/pojodojo/controller/TheController.java
@@ -2,7 +2,6 @@ package com.tidcode.pojodojo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
